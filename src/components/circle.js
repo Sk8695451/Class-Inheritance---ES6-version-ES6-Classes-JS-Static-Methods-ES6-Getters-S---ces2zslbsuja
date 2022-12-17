262c740b6a8f0f4d832c7a8352ca13e45b133eff
unknown <sk8695451>
2022-12-18 04:19:42 +0530

    first

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
-// import using require
-
-// declare class
-
-
-// export class using module.exports
+var Shape = require('./Shape'); 
+  
+ // declare class 
+ class Circle extends Shape{ 
+     constructor(name){ 
+         super(name); 
+  
+     } 
+  
+     calculateArea(radius){ 
+         return Math.PI * Math.pow(radius,2); 
+     } 
+      
+ } 
+  
+ module.exports=Circle; 
